3e8c9f3eb5788312f2d42e2031ad44fe1da2d8ca
Paul_K <p.keptene@gmail.com>
2026-01-18 15:27:07 +0200

    Solution 2

--- app/main.py
+++ app/main.py
@@ -1,21 +1,34 @@
-from typing import Any
-
-
 def get_human_age(cat_age: int, dog_age: int) -> list:
-    list_with_ages: list[Any] = []
+    max_age = 200
+    cat_age_divider = 4
+    dog_age_divider = 5
 
-    if cat_age < 15:
-        list_with_ages.append(0)
-    elif cat_age < 24:
-        list_with_ages.append(1)
-    else:
-        list_with_ages.append(2 + ((cat_age - 24) // 4))
+    def validate_age(age: int, animal_name: str) -> int | None:
+        if not isinstance(age, (int, float)):
+            raise TypeError(f"{animal_name} age must be a number")
+        age = int(age)
 
-    if dog_age < 15:
-        list_with_ages.append(0)
-    elif dog_age < 24:
-        list_with_ages.append(1)
-    else:
-        list_with_ages.append(2 + ((dog_age - 24) // 5))
+        if age < 0:
+            raise ValueError(f"{animal_name} age cannot be negative")
 
-    return list_with_ages
+        if age > max_age:
+            raise OverflowError(
+                f"{animal_name} age is unrealistically large"
+            )
+        return age
+
+    def calculate_age(animal_age: int, special_divider: int) -> int:
+        if animal_age < 15:
+            return 0
+        elif animal_age < 24:
+            return 1
+        else:
+            return 2 + ((animal_age - 24) // special_divider)
+
+    cat_age_valid = validate_age(cat_age, "Cat")
+    dog_age_valid = validate_age(dog_age, "Dog")
+
+    return [
+        calculate_age(cat_age_valid, cat_age_divider),
+        calculate_age(dog_age_valid, dog_age_divider)
+    ]
--- app/test_main.py
+++ app/test_main.py
@@ -2,22 +2,45 @@ import pytest
 from app.main import get_human_age
 
 
+def test_not_int_values() -> None:
+    with pytest.raises(TypeError):
+        get_human_age(2, "3")
+
+
+def test_below_zero_values() -> None:
+    with pytest.raises(ValueError):
+        get_human_age(-10, 0)
+
+
+def test_unrealistic_values() -> None:
+    with pytest.raises(OverflowError):
+        get_human_age(1000, 1000)
+
+
 @pytest.mark.parametrize(
     "cat_age, dog_age, result",
     [
         (0, 0, [0, 0]),
         (14, 14, [0, 0]),
         (15, 15, [1, 1]),
+        (16, 16, [1, 1]),
+        (23, 23, [1, 1]),
         (24, 24, [2, 2]),
-        (28, 28, [3, 2]),
+        (25, 25, [2, 2]),
+        (27, 28, [2, 2]),
+        (28, 29, [3, 3]),
         (100, 100, [21, 17]),
     ],
     ids=[
         "All zeroes",
         "All less than 15",
         "All 15",
+        "All 16",
+        "All 23",
         "All 24",
-        "All 28",
+        "All 25",
+        "27 and 28",
+        "28 and 29",
         "All 100",
     ]
 )
